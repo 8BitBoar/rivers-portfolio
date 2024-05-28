6808959e294fedb36a8db81434900551c15c2075
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-27 21:30:16 -0400

    testing styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,24 +3,24 @@ import React from "react";
 export default function Contact() {
     return (
         <section id="contact" className="relative">
-            <div className="container px-5 py-10 mx-auto flex-wrap grid-cols-2">
+            <div className="container px-5 py-10 mx-auto flex justify-center grid-cols-2">
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
+                    <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                         LinkedIn
                     </h2>
                 </div>
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
+                    <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                         Email
                     </h2>
                 </div>
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
+                    <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                         Phone Number
                     </h2>
                 </div>
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
+                    <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
                         GitHub
                     </h2>
                 </div>
