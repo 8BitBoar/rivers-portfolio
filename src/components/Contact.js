07d1139aa5d738a10708aeb508c37507e092f108
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-27 21:33:15 -0400

    more styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,9 @@ export default function Contact() {
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex justify-center grid-cols-2">
                 <div>
+                    <h2 className="title-font font-semibold text-white tracking-widest text-xs">LinkedIn</h2>
                     <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                        LinkedIn
+                        in/sriversgreen
                     </h2>
                 </div>
                 <div>
