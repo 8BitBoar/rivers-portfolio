94d624bfe45c959b0ab7e13229fe480f2e4624db
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-28 10:49:41 -0400

    more styling for contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,7 +19,7 @@ export default function Contact() {
                     </h2>
                 </div>
                 <div className="ml-4 flex-col text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none rounded text-lg">
-                    <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                    <h2 className="title-font font-semibold text-white tracking-widest text-400">
                         Phone Number
                     </h2>
                     <h2>
@@ -27,11 +27,11 @@ export default function Contact() {
                     </h2>
                 </div>
                 <div className="ml-4 flex-col text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none rounded text-lg">
-                    <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                    <h2 className="title-font font-semibold text-white tracking-widest text-400">
                         GitHub
                     </h2>
-                    <h2 className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                        <a href="https://github.com/8BitBoar">https://github.com/8BitBoar</a>
+                    <h2 className="hover:bg-gray-700 hover:text-white">
+                        <a href="https://github.com/8BitBoar">8BitBoar</a>
                     </h2>
                 </div>
           </div>
