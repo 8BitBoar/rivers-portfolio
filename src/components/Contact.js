deb1d6a94f57f27a237445992d2fae4ebcb4173f
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-27 21:26:42 -0400

    figuring out styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,24 +3,24 @@ import React from "react";
 export default function Contact() {
     return (
         <section id="contact" className="relative">
-            <div className="container px-5 py-10 mx-auto flex-wrap">
+            <div className="container px-5 py-10 mx-auto flex-wrap grid-cols-2">
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
                         LinkedIn
                     </h2>
                 </div>
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
                         Email
                     </h2>
                 </div>
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
                         Phone Number
                     </h2>
                 </div>
                 <div>
-                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font rounded-md text-gray-400 bg-gray-800">
                         GitHub
                     </h2>
                 </div>
