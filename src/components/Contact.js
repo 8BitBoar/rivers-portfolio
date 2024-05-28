84d1dd66f0e66e7920b25fa454edd62e03a1aa1d
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-28 11:04:14 -0400

    more styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function Contact() {
     return (
         <section id="contact" className="relative">
-            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font place-content-center">
                 Contact Me
             </h2>
             <div className="container px-5 py-10 mx-auto justify-center grid grid-cols-2 gap-4">
                 <div className="ml-4 flex-col text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none rounded text-lg">
                     <h2 className="title-font font-semibold text-white tracking-widest text-400">LinkedIn</h2>
-                    <h2>
-                        in/sriversgreen
+                    <h2 className="hover:bg-gray-700 hover:text-white">
+                        <a href="www.linkedin.com/in/sriversgreen">in/sriversgreen</a>
                     </h2>
                 </div>
                 <div className="ml-4 flex-col text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none rounded text-lg">
