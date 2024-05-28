2d4978d5af123565ef5559e6cd3f1dd435e495bf
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-28 11:15:21 -0400

    testing scroll to top

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects"
 import Skills from "./components/Skills";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Projects />
       <Skills />
       <Contact />
+      <ScrollToTop />
     </main>
   );
 }
