09a8079bf405da475a0fccff186b82f556b19087
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-27 21:21:09 -0400

    testing flex

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,27 +3,27 @@ import React from "react";
 export default function Contact() {
     return (
         <section id="contact" className="relative">
-            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-            <div>
-                <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                    LinkedIn
-                </h2>
-            </div>
-            <div>
-                <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                    Email
-                </h2>
-            </div>
-            <div>
-                <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                    Phone Number
-                </h2>
-            </div>
-            <div>
-                <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                    GitHub
-                </h2>
-            </div>
+            <div className="container px-5 py-10 mx-auto flex-wrap">
+                <div>
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        LinkedIn
+                    </h2>
+                </div>
+                <div>
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        Email
+                    </h2>
+                </div>
+                <div>
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        Phone Number
+                    </h2>
+                </div>
+                <div>
+                    <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                        GitHub
+                    </h2>
+                </div>
           </div>
         </section>
       );
