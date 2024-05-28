888c48ba0c9e6b827b476365b9a0e5c05c4aa457
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-27 21:11:32 -0400

    import statements

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,16 @@
+import React from "react";
+
 export default function Contact() {
     return (
         <section id="contact" className="relative">
           <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+          <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                LinkedIn
+            </h2>
+            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
+                Email
+              </h2>
+
             <form
               netlify
               name="contact"
