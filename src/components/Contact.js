621bacf64c47fc2c03bd1c0392d9657f94823485
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-05-28 10:04:32 -0400

    flex col

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,8 @@ export default function Contact() {
     return (
         <section id="contact" className="relative">
             <div className="container px-5 py-10 mx-auto flex justify-center">
-                <div className="ml-4 flex-col text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                <div className="ml-4 flex-col text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none rounded text-lg">
                     <h2 className="title-font font-semibold text-white tracking-widest text-xs">LinkedIn</h2>
-                    <br className="hidden lg:inline-block" />
                     <h2>
                         in/sriversgreen
                     </h2>
