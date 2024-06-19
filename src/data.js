654c901f6e4c21c41166b110964c125dbd999fad
8BitBoar <106558110+8BitBoar@users.noreply.github.com>
2024-06-19 17:34:17 -0400

    Hobby Project added

--- src/data.js
+++ src/data.js
@@ -35,36 +35,12 @@ export const projects = [
 
 export const hobbyProjects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
+    title: "Project Zomboid Profession Mod",
+    subtitle: "Lua",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "Creates a custom profession for the game based on axe wielding nomadic players.",
+    image: "./project-h-zomboid-background.png",
+    link: "https://github.com/8BitBoar/NomadicLumberjackProfession",
   },
 ];
 
